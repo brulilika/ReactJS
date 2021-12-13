9c38495239d9d890e7d3c85532dfb7b57c00de5d
Bruna Lika <brunalika@msn.com>
2021-12-13 09:57:01 -0300

    copia projeto passado

--- 3-React_EntendendoBiblioteca/ceep/src/App.js
+++ 3-React_EntendendoBiblioteca/ceep/src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
     const novoArrayNotas = [...this.state.notas,novaNota]
     const novoEstado = {
       notas:novoArrayNotas
+      
     }
     this.setState(novoEstado)
   }
