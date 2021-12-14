1d2e14c6777e420cb9b4ab0de6d9b6ab775e4279
Bruna Lika <brunalika@msn.com>
2021-12-14 15:00:35 -0300

    adição de validacao unfocused cpf

--- 5-FunctionComponents/formulario/src/App.js
+++ 5-FunctionComponents/formulario/src/App.js
@@ -9,10 +9,23 @@ function App() {
   return (
     <Container component="article" maxwidth="sm">
       <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
-      <FormularioCadastro />
+      <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCpf}/>
     </Container>
     
   );
+}
+
+function aoEnviarForm(dados){
+
+}
+
+function validarCpf(cpf){
+  if(cpf.length !== 11){
+    return {erro:true, texto: "CPF deve ter 11 dígitos"}
+  }
+  else{
+    return {erro:false, texto: ""}
+  }
 }
 
 export default App;
--- 5-FunctionComponents/formulario/src/componentes/Formulario/Formulario.jsx
+++ 5-FunctionComponents/formulario/src/componentes/Formulario/Formulario.jsx
@@ -1,17 +1,21 @@
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core';
 import React, {useState} from 'react';
 
-function FormularioCadastro(){
+//descontrucao do proprio parametro
+function FormularioCadastro({aoEnviar, validarCPF}){
+
     //variavel, funcao para ajustar o estado
     const[nome, setNome] = useState("");
     const[sobrenome, setSobrenome] = useState("");
     const[cpf, setCPF] = useState("");
-
+    const[promocao, setPromocaoo] = useState(false);
+    const[novidade, setNovidade] = useState(false);
+    const[erros, setErros] = useState({cpf: {erro: false, texto:""}});
 
     return (
         <form onSubmit={e => {
             e.preventDefault();
-            console.log(nome);
+            aoEnviar(nome,sobrenome,cpf,promocao, novidade);
         }}>
             <TextField id="nome" value={nome}  label="Nome" variant="outlined" margin="normal" fullWidth
                         onChange={e => {
@@ -21,11 +25,28 @@ function FormularioCadastro(){
                         onChange={e => {
                             setSobrenome(e.target.value);
                         }}/>
-            <TextField id="cpf" value={cpf} label="CPF" variant="outlined" margin="normal" fullWidthonChange={e => {
-                            setCPF(e.target.value);
-                        }}/>
-            <FormControlLabel label="Promoções" control={<Switch name="Promoções" defaultChecked/>}/>
-            <FormControlLabel label="Novidades" control={<Switch name="Novidades" defaultChecked/>}/>
+            <TextField  id="cpf" value={cpf} label="CPF" variant="outlined" margin="normal" 
+                        error={erros.cpf.erro} helperText={erros.cpf.texto}
+                        onChange={e => {
+                            setCPF(e.target.value); 
+                        }}
+                        onBlur={e =>{
+                            setErros({cpf:validarCPF(cpf)})
+                        }}
+                       />
+
+            <FormControlLabel label="Promoções" control={
+                <Switch name="Promoções" checked={promocao}
+                    onChange={e =>{
+                        setPromocaoo(e.target.checked)
+                    }}
+                />
+                } 
+            />
+            <FormControlLabel label="Novidades" control={<Switch name="Novidades" checked={novidade} 
+                              onChange={e =>{
+                                setNovidade(e.target.checked)
+                               }}/>} />
             <Button type="submit" variant="contained" color="primary">Enviar</Button>
         </form>
     );
