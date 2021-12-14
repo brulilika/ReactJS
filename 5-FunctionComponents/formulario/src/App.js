d5953c3ed8ce01c269bec30ec1f37b2200273626
Bruna Lika <brunalika@msn.com>
2021-12-14 10:11:52 -0300

    estrutura inicial forms + material-ui

--- 5-FunctionComponents/formulario/src/App.js
+++ 5-FunctionComponents/formulario/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import FormularioCadastro from './componentes/Formulario/Formulario'
+
+/*A criação de componentes react por meio de functions é algo novo*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Pao</h1>
+      <FormularioCadastro />
+    </section>
+    
   );
 }
 
