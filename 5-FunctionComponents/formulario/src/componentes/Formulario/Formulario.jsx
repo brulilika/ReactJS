c4ed776e365cb9caae730275bdbc21fced3af20e
Bruna Lika <brunalika@msn.com>
2021-12-14 12:41:18 -0300

    inicio de uso de hooks

--- 5-FunctionComponents/formulario/src/componentes/Formulario/Formulario.jsx
+++ 5-FunctionComponents/formulario/src/componentes/Formulario/Formulario.jsx
@@ -1,12 +1,29 @@
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core';
-import React from 'react';
+import React, {useState} from 'react';
 
 function FormularioCadastro(){
+    //variavel, funcao para ajustar o estado
+    const[nome, setNome] = useState("");
+    const[sobrenome, setSobrenome] = useState("");
+    const[cpf, setCPF] = useState("");
+
+
     return (
-        <form>
-            <TextField id="nome" label="Nome" variant="outlined"  margin="normal" fullWidth/>
-            <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth/>
-            <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth/>
+        <form onSubmit={e => {
+            e.preventDefault();
+            console.log(nome);
+        }}>
+            <TextField id="nome" value={nome}  label="Nome" variant="outlined" margin="normal" fullWidth
+                        onChange={e => {
+                            setNome(e.target.value);
+                        }}/>
+            <TextField id="sobrenome" value={sobrenome} label="Sobrenome" variant="outlined" margin="normal" fullWidth
+                        onChange={e => {
+                            setSobrenome(e.target.value);
+                        }}/>
+            <TextField id="cpf" value={cpf} label="CPF" variant="outlined" margin="normal" fullWidthonChange={e => {
+                            setCPF(e.target.value);
+                        }}/>
             <FormControlLabel label="Promoções" control={<Switch name="Promoções" defaultChecked/>}/>
             <FormControlLabel label="Novidades" control={<Switch name="Novidades" defaultChecked/>}/>
             <Button type="submit" variant="contained" color="primary">Enviar</Button>
