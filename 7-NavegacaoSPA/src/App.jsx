379a83b0bc98bb856edb455b0aac29551c5c51ae
Bruna Lika <brunalika@msn.com>
2021-12-20 18:07:22 -0300

    primeiras rotas

--- 7-NavegacaoSPA/src/App.jsx
+++ 7-NavegacaoSPA/src/App.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
+import Pagina404 from './paginas/Page404'
 
 function App() {
-  const Router = () => {
-    const location = window.location.pathname
-    if(location === '/sobre')
-      return <Sobre/>
-    else
-      return <Home/>
-  }
+  
   return (
-  <>
-    {Router()}
-  </>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route path='/sobre'>
+            <Sobre/>
+          </Route>
+          <Route>
+            <Pagina404/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+    </>
   )
 }
 
