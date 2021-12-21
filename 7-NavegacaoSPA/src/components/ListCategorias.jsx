ef0165ebd451b126d74a3c8aa6be35df2fd5126e
Bruna Lika <brunalika@msn.com>
2021-12-21 10:21:11 -0300

    lista de categorias

---
+++ 7-NavegacaoSPA/src/components/ListCategorias.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom'
+import {buscaPost} from '../api/api'
+import '../assets/css/blog.css'
+
+function ListaCategorias({url}){
+    const[categorias, setCategorias] = useState([])
+
+    useEffect(() =>{
+        buscaPost('/categorias',setCategorias)
+    }, [])
+
+    return(
+        <ul className="lista-categorias container flex">
+            {
+            categorias.map((categoria) => (
+                <Link to={`categoria/${categoria.id}`}>
+                    <li className={`lista-categorias__categoria lista-categorias__categoria--${categoria.id}`}>
+                        {categoria.nome}
+                    </li>
+                </Link>
+            ))
+            }
+        </ul>
+    )
+}
+
+export default ListaCategorias
