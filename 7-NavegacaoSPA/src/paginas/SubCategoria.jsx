ecddca3a59dd261079c3c606b995c835204738a3
Bruna Lika <brunalika@msn.com>
2021-12-21 16:02:42 -0300

    botoes de subcategorias

---
+++ 7-NavegacaoSPA/src/paginas/SubCategoria.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from 'react-router-dom'
+import ListaPost from "../components/ListPost";
+
+function SubCategoria(){
+    const { subcategoria } = useParams()
+    return(
+        <ListaPost url={`/posts?subcategoria=${subcategoria}`} />
+    )
+}
+
+export default SubCategoria
